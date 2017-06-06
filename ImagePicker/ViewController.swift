b6b8c150528dff8f903fbc2103a1d440f0a39604
Shariif Islam <islamshariif@gmail.com>
2017-06-06 16:18:55 +0600

    Push to main

--- ImagePicker/ViewController.swift
+++ ImagePicker/ViewController.swift
@@ -1,25 +1,57 @@
-//
-//  ViewController.swift
-//  ImagePicker
-//
-//  Created by AdBox on 6/6/17.
-//  Copyright © 2017 myth. All rights reserved.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var iv_image: UIImageView!
+    @IBAction func pickImage(_ sender: Any) {
+        
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        
+        let actionSheet = UIAlertController(title: "Image option", message: "Choose an option", preferredStyle: .actionSheet)
+
+        actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: { (action) in
+            
+            if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            
+                imagePicker.sourceType = .camera
+                self.present(imagePicker, animated: true, completion: nil)
+            }
+            else {
+            
+                let alert = UIAlertController(title: "Warning", message: "No camera available", preferredStyle: .alert)
+                
+                alert.addAction(UIAlertAction(title: "Ok", style: .cancel, handler: nil))
+                
+                self.present(alert, animated: true, completion: nil)
+            }
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Photo Libary", style: .default, handler: { (action) in
+            imagePicker.sourceType = .photoLibrary
+            self.present(imagePicker, animated: true, completion: nil)
+            
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        self.present(actionSheet, animated: true, completion: nil)
     }
+}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+extension ViewController : UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            self.iv_image.image = image
+        }
+        picker.dismiss(animated: true, completion: nil)
     }
-
-
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
 }
 
